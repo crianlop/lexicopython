d0dd840d2ef3ace409dccb6f84412b9046be1717
crianlop <crianlop@espol.edu.ec>
2020-11-13 17:59:47 -0500

    inicial commit

---
+++ lexicoPython.py
@@ -0,0 +1,67 @@
+# GRUPO PYTHON
+import ply
+import ply.lex as lex
+import ply.yacc as yacc
+
+
+
+reserved = {
+    "if": "IF",
+    "then": "THEN",
+    "else": "ELSE",
+    "while": "WHILE",
+    "and" : "AND",
+    "not" : "NOT",
+    "is" : "IS"
+}
+tokens = ("ID","EQUALS","ASSIGN","NUMBER",
+          "PLUS","MINUS","TIMES","DIVIDE",
+          "NOT","LESSTHAN","MORETHAN","LPAREN","RPAREN")
+t_PLUS = r'\+'
+t_MINUS = r'-'
+t_TIMES = r'\*'
+t_DIVIDE = r'/'
+t_LPAREN = r'\('
+t_RPAREN = r'\)'
+t_EQUALS = r'\=='
+t_ASSIGN = r'\='
+t_NOT = r'\!'
+t_LESSTHAN = r'\<'
+t_MORETHAN = r'\>'
+t_COLON = r':'
+
+
+def t_NUMBER(t):
+    r'\d+'
+    t.value = int(t.value)
+    return t
+def t_ID(t):
+    r'[a-zA-Z_][a-zA-Z_0-9]*'
+    t.type = reserved.get(t.value,'ID')    # Check for reserved words
+    return t
+t_ignore  = ' \t'
+def t_newline(t):
+    r'\n+'
+    t.lexer.lineno += len(t.value)
+def t_error(t):
+    print("Illegal character '%s'" % t.value[0])
+    t.lexer.skip(1)
+
+
+lexer = lex.lex()
+
+data = '''
+3 + 4 * 10
+  + -20 *2
+  _hola
+'''
+
+# Give the lexer some input
+lexer.input(data)
+
+# Tokenize
+while True:
+    tok = lexer.token()
+    if not tok:
+        break  # No more input
+    print(tok)
